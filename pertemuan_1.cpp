4169774a8d92e1d1406ea587e0a69c7fca0216e6
andreskak <55776334+andreskak@users.noreply.github.com>
2021-10-23 04:34:13 +0700

    c++
    Langkah Langkah 
    1.Install vscode
    2.masuk extension menu
    3.install code runner dan c/c++

---
+++ pertemuan_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    main(){
+        //cout fungsinya untuk mengeluarkan (print)
+    cout<<"************************"<< endl;
+    cout<<"*    TUGAS ALGORITMA   *"<< endl;
+    cout<<"*                      *"<< endl;
+    cout<<"*                      *"<< endl;
+    cout<<"************************"<< endl;
+    cout<<"Nama : Nisrina Salma"<< endl;
+    cout<<"Saya Mahasiswa Semester 1 Ilmu Komputer"<<endl;
+    
+}
