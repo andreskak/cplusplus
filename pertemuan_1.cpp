06f4af602ad40640947153be3854fd9a25ca78bf
andreskak <55776334+andreskak@users.noreply.github.com>
2021-10-23 18:18:40 +0700

    Update pertemuan_1.cpp

--- pertemuan_1.cpp
+++ pertemuan_1.cpp
@@ -1,8 +1,14 @@
+// ini adalah library
+//salin kode ini lalu masukan ke vscode atau bisa di download
 #include <iostream>
 using namespace std;
 
     main(){
-        //cout fungsinya untuk mengeluarkan (print)
+        //cout<< fungsinya untuk mengeluarkan (print)
+        //tanda kutip " " dalam cout adalah fungsi untuk mengeprint string
+        // "//" dalam c++ tidak akan di ekseksui dan di anggap hanya sebagai mark/noted
+        //endl; adalah end line artinya jika code sudah di baca maka akan membuat garis baru
+        
     cout<<"************************"<< endl;
     cout<<"*    TUGAS ALGORITMA   *"<< endl;
     cout<<"*                      *"<< endl;
